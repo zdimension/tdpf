36c27a87c0e15f1a3b9a5046f178e3e97a2c1506
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2022-04-20 10:02:52 +0200

    Fix moodle

--- TD9/Exo1.rkt
+++ TD9/Exo1.rkt
@@ -1,13 +1,14 @@
 (load "eval.ss")
 
-(let ((init-orig init-interpreter)
-      (*top* #f))
-
+(let ((init-orig init-interpreter) (*top* #f))
   (set! init-interpreter
         (lambda()
           (init-orig)
           (call/cc (lambda(k) (set! *top* k)))))
 
-  (set! ms-error (lambda(x y)
-                   (printf "~a ~a~n" x y)
-                   (*top*))))
+  (set! ms-error (lambda args
+                   (printf "*** ERROR in Mini-Scheme: ~a\n"
+                           (string-join (map (lambda (x) (format "~a" x)) args) " "))
+                   (*top* 'unused)))
+
+  (set-binding! 'error ms-error *global-env* #f))
--- TD9/Exo2.rkt
+++ TD9/Exo2.rkt
@@ -1,13 +1,15 @@
 (load "Exo1.rkt")
 
-(let ((interp-orig interp)
-      (*top* #f))
-
-  (set-binding! 'exit (lambda() (*top*)) *global-env* #f)
+(let ((interp-orig interp) (eval-old evaluate) (*top* #f))
+  (set-binding! 'exit (lambda() (*top* 'unused)) *global-env* #f)
 
   (set! interp
         (lambda()
           (call/cc (lambda(k)
                      (set! *top* k)
-                     (interp-orig)
-                     )))))
+                     (interp-orig)))))
+
+  (set! evaluate (lambda (expr env)
+                   (if (and (pair? expr) (eq? 'λ (car expr)))
+                       (eval-old (cons 'lambda (cdr expr)) env)
+                       (eval-old expr env)))))
