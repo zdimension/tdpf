eaf1847965cb38b6f5595367ce569019f97a816c
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2022-04-20 20:39:24 +0200

    Update Exo7.rkt

--- TD8/Exo7.rkt
+++ TD8/Exo7.rkt
@@ -1,25 +1,33 @@
 (define-macro (make-generator  . body)
-    `(let ()
-       (define return #f)
-       (define resume #f)
-       (define (yield value)
-         (call/cc (lambda (k) (set! resume k) (return value))))
+  `(let ()
+     (define return #f)
+     (define resume #f)
+     (define (yield value)
+       (call/cc (lambda (k) (set! resume k) (return value))))
 
-       ;; le résultat est une fonction qui capture return, resume et yield
-       (λ ()
-         (call/cc (λ (k)
-                    (if resume
-                     (resume)
-                     (begin
-                       (set! return k)
-                       ,@body
-                       '*END*)))))))
+     ;; le résultat est une fonction qui capture return, resume et yield
+     (λ ()
+       (call/cc (λ (k)
+                  (set! return k)
+                  (if resume
+                      (resume 'unused)
+                      (begin
+                        ,@body
+                        '*END*)))))))
 
 (define G (make-generator
-               (yield 'foo)
-               (yield 'bar)))
+           (yield 'foo)
+           (yield 'bar)))
+(G)
+(G)
+(G)
+(G)
 
-(G)
-(G)
-(G)
-(G)
+(define fib (make-generator
+             (let Loop ((i 0) (j 1))
+               (yield i)
+               (Loop j (+ i j)))))
+(let Loop ((n 0))
+  (when (< n 10)
+    (printf "Fib(~s)= ~s\n" n (fib))
+    (Loop (+ n 1))))
